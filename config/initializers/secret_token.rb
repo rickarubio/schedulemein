4ad86fc240664b091958505f8d4e8fc9306c2381
Ricardo Rubio <rickarubio@gmail.com>
2014-03-21 16:24:23 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schedulemein::Application.config.secret_token = '0631693df189e8631ef5b3f32ac4adf37703f0e0d3b8e9d13387030c128633aeb67136a9b74b3696aee1edc307fc2ba381d0c52abab2af4380f36d6140d5355d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Schedulemein::Application.config.session_store :cookie_store, key: '_schedulemein_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Schedulemein::Application.config.session_store :active_record_store
