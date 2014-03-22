e8fca8a98f0ca3b1e9601983202e9b2bd283db91
Ricardo Rubio <rickarubio@gmail.com>
2014-03-21 17:56:51 -0700

    added users_controller file and first users controller failing test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe UsersController do
   context "#index" do
+    it "is successful" do
+      get :index
+      expect(response).to be_success
+    end
   end
 
   context "#show" do
