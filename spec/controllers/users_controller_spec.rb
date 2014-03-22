bdf1f33f721e0df7419b232099a3eea870eb5be5
Ricardo Rubio <rickarubio@gmail.com>
2014-03-21 17:51:16 -0700

    stubs out rspec tests for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe UsersController do
+  context "#index" do
+  end
 
+  context "#show" do
+  end
+
+  context "#new" do
+  end
+
+  context "#create" do
+  end
+
+  context "#edit" do
+  end
+
+  context "#update" do
+  end
+
+  context "#destroy" do
+  end
 end
